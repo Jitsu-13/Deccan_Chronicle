0043613c8d9c0f0bc91d7d0a93f50c6488c66779
Jitsu-13 <yadav_sujit@outlook.com>
2022-10-04 14:52:45 +0530

    technology Page

---
+++ technology.js
@@ -0,0 +1,227 @@
+let left_data=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-liq2jpbp0j2q8jchgqa8tn9e82-20210624162756.jpeg`,
+        headline:"WhatsApp bans over 23.28 lakh Indian accounts in August",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-cdg69uinab6ahnm4rt1qmbr6s1-20220926120352.jpeg`,
+        headline:"Apple set to manufacture its latest iPhone 14 in India",
+        details:"Apple unveiled its highly anticipated iPhone 14 and the iPhone 14 Plus variant during its 'Far Out' event on September 7"
+    },
+]
+
+let center_data=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-u1k41kks0sdnt8h85pgen5qq67-20220925121920.jpeg`,
+        headline:"Goa daily wage worker builds robot to feed his differently-abled daughter"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-d51mondbaernmd7f725ddgo6h7-20210312010311.jpeg`,
+        headline:"Apple addresses camera shake issue on new iPhone Pro models with iOS 16.0.2 update"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-hs44g1gierq776h4ds8v2bh0p0-20220922113951.jpeg`,
+        headline:"Here's the real reason to turn on aeroplane mode when you fly"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-g4bflmnbi4coegebsi9036ohp2-20200825163909.jpeg`,
+        headline:"Computer security experts warn against loopholes in Zoom app"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-fikjf1h3p5cpgp0a5ldog6ui60-20170526015144.jpeg`,
+        headline:"US Treasury recommends exploring creation of a digital dollar"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-sjbi1m2t5drinidtkmqvnftoq4-20220918170744.jpeg`,
+        headline:"Is Ethereum ditching its ‘miners’"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-p7gblobppvmu7vlufl9hmuc851-20200506111907.jpeg`,
+        headline:"EU wants to toughen cybersecurity rules for smart devices"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-sjbi1m2t5drinidtkmqvnftoq4-20220918185412.jpeg`,
+        headline:`Loop Energy says new hydrogen fuel cell more efficient than diesel engine`,
+    },
+]
+
+let right_data=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-c6vcqtlfu5qn38at3rp993fd81-20200226145043.jpeg`,
+        headline:"Audio-Technica ATH-CLR100BT: Best 'Big-Name’ budget ‘earphones!"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-c6vcqtlfu5qn38at3rp993fd81-20200224061540.jpeg`,
+        headline:`Realme C3 review: Premium performance, mind-blowing price!`
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-c6vcqtlfu5qn38at3rp993fd81-20200223092702.jpeg`,
+        headline:"ColorOS 7 review: Slick, smooth and everything in between!"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-c6vcqtlfu5qn38at3rp993fd81-20200222092056.jpeg`,
+        headline:`Belkin RockStar headphones with Lightning review: A better Apple EarPods alternative!`
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-c6vcqtlfu5qn38at3rp993fd81-20200221150312.jpeg`,
+        headline:"JVC HA-A10T review: Great fit, awesome sound, but that's it!"
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-c6vcqtlfu5qn38at3rp993fd81-20200217120428.jpeg`,
+        headline:"Jabra Elite 75t review: An AirPods Pro killer!"
+    },
+]
+
+let drop_1=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-c6vcqtlfu5qn38at3rp993fd81-20200212065053.jpeg`,
+        headline:"Samsung begins pre-booking Galaxy Z Flip in India; prices it just right",   
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-4v24huvehu1ggor0uk567vnju0-20211007095354.jpeg`,
+        headline:"New Google Pixel 6 leak teases Magic Eraser, device to get five years of updates",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-0oh9vvs9288dmj8io1b3nkmhg4-20220114111035.jpeg`,
+        headline:"Realme 9i to be launched in India on January 18",
+    },
+]
+let drop_1_1=[
+    {
+        para:`Apple set to manufacture its latest iPhone 14 in India`
+    },
+    {
+        para:`Beware of video calls from stranger, warn police`
+    },
+    {
+        para:`Google Pixel 6, 6 Pro launch commercials leaked prior to official unveiling`
+    },
+]
+let drop_2=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-erno58a0h8mun8j68mnngdidc7-20211201125855.jpeg`,
+        headline:"Oppo Reno7, Reno7 Pro India price leaks ",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-niaiiuo332tf0dti0cauos9n46-20210821163628.jpeg`,
+        headline:"Google Pixel 6, Pixel 6 Pro may launch on September 13 ",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-ctf0fn8jmimeuht7sugco44fj6-20200907085824.jpeg`,
+        headline:"Samsung Galaxy Tab S8 Ultra's leaked renders confirm notched display ",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-g4bflmnbi4coegebsi9036ohp2-20200815151636.jpeg`,
+        headline:"Amazon planning to launch Alexa-powered TV in October ",
+    },
+]
+let drop_2_2=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-4j1h2oas9ctplr1ntgm5l2cis4-20181107105211.jpeg`,
+        headline:"Samsung Galaxy Tab S8 lineup's colour options, memory configurations out",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-7sthnv1racnkcspufjmsq7r465-20210922203115.jpeg`,
+        headline:"HP's new 11-inch tablet comes with flipping webcam",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-hs44g1gierq776h4ds8v2bh0p0-20220922113951.jpeg`,
+        headline:"Here's the real reason to turn on aeroplane mode when you fly",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-g63padcnfttl6cd653skqm7fi5-20211027191053.jpeg`,
+        headline:"Pixel 6, Pixel 6 Pro will arrive with a day-one update",
+    },
+]
+let drop_4=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-fikjf1h3p5cpgp0a5ldog6ui60-20170526015144.jpeg`,
+        headline:"US Treasury recommends exploring creation of a digital dollar ",   
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-gm2biang516qp9ons8tnal92m3-20180417085244.jpeg`,
+        headline:"Netflix tests TikTok-style feed for children ",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-d51mondbaernmd7f725ddgo6h7-20210508002009.jpeg`,
+        headline:"WhatsApp working on message reactions for iOS, Android ",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-0oh9vvs9288dmj8io1b3nkmhg4-20220112104208.jpeg`,
+        headline:"Nokia Lite Earbuds, Wired Buds launch in India ",
+    },
+]
+
+let drop_4_4=[
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-q1ekml21ovukplfqqhcs8jlv83-20211029002330.jpeg`,
+        headline:"Facebook Messenger to get built-in bill splitting feature",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-8i6cov2nia0vn21ol6b5690i60-20220128115501.jpeg`,
+        headline:"Google begins rolling new pitch-black dark mode",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-d4de1jkac2kauu3v23hnu5r336-20220827110828.jpeg`,
+        headline:"Twitter adds “Edit Tweet” button, finally",
+    },
+    {
+        img:`https://s3.ap-southeast-1.amazonaws.com/images.deccanchronicle.com/dc-Cover-liq2jpbp0j2q8jchgqa8tn9e82-20210624162756.jpeg`,
+        headline:"WhatsApp bans over 23.28 lakh Indian accounts in August",
+    },
+]
+
+import { appendLeft,appendCenter,appendRight,appendD1,appendD1_1,appendD2,appendD2_2,appendD3,appendD3_3} from "./append_technology.js";
+import navbar from './componets/navbar.js';
+
+let nav=document.getElementById('navbar');
+
+nav.innerHTML=navbar();
+
+const left_show=()=>{
+    let container=document.getElementById("left");
+    appendLeft(left_data,container);
+}
+left_show()
+const middle_show=()=>{
+    let container=document.getElementById("middle");
+    appendCenter(center_data,container)
+}
+middle_show()
+const right_show=()=>{
+    let container=document.getElementById("right_inner");
+    appendRight(right_data,container)
+}
+right_show()
+const d1_show=()=>{
+    let container=document.getElementById("d1_inner");
+    appendD1(drop_1,container)
+}
+d1_show()
+const d1_1_show=()=>{
+    let container=document.getElementById("d1_outer");
+    appendD1_1(drop_1_1,container)
+}
+d1_1_show()
+const d2_show=()=>{
+    let container=document.getElementById("d2_inner");
+    appendD2(drop_2,container)
+}
+d2_show()
+const d2_2_show=()=>{
+    let container=document.getElementById("d2_outer");
+    appendD2_2(drop_2_2,container)
+}
+d2_2_show()
+const d3_show=()=>{
+    let container=document.getElementById("d4_inner");
+    appendD3(drop_4,container)
+}
+d3_show()
+const d3_3_show=()=>{
+    let container=document.getElementById("d4_outer");
+    appendD3_3(drop_4_4,container)
+}
+d3_3_show()
+
+ 
